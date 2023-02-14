594a6009b0417804e00cae11d477885766a57869
George Correa <georgecorrea12@gmail.com>
2023-02-13 23:26:27 -0600

    push 2

--- scripts/joins_exercise_gattstacks.sql
+++ scripts/joins_exercise_gattstacks.sql
@@ -24,11 +24,39 @@ INNER JOIN revenue
 ON specs.movie_id = revenue.movie_id
 ORDER BY worldwide_gross
 LIMIT 1;
-
+	-- Semi-Tough (1977), $37,187,139
 
 -- 2. What year has the highest average imdb rating?
 
+SELECT
+	release_year,
+	AVG(imdb_rating)
+FROM specs
+INNER JOIN rating
+ON specs.movie_id = rating.movie_id
+GROUP BY specs.release_year
+ORDER BY AVG(imdb_rating) DESC
+LIMIT 1;
+	-- 1991 with average imdb rating of 7.45
+
 -- 3. What is the highest grossing G-rated movie? Which company distributed it?
+
+SELECT 
+	film_title,
+	mpaa_rating,
+	worldwide_gross,
+	company_name
+FROM 
+	specs
+INNER JOIN
+	revenue,
+	distributors
+ON
+	specs.movie_id = revenue.movie_id
+	specs.domestic_distributor_id = distributors.distributor_id
+ORDER BY
+	worldwide_gross DESC
+LIMIT 1;
 
 -- 4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies 
 -- table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
